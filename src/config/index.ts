a58a32a910e12c9837df12bdbe28be52a24c9ee3
S  M  Mofassel  Hosain <108587547+Mofassel100@users.noreply.github.com>
2023-10-07 19:27:50 +0600

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,7 +13,10 @@ const envVarsZodSchema = z.object({
   JWT_SECRET: z.string(),
   REDIX_URL: z.string(),
   AUTH_SERVICE_URL: z.string(),
-  COR_SERVICE_URL: z.string()
+  COR_SERVICE_URL: z.string(),
+    CLOUDINARY_CLOUD_NAME: z.string(),
+    CLOUDINARY_API_KEY: z.string(),
+    CLOUDINARY_API_SECRET: z.string()
 });
 
 const envVars = envVarsZodSchema.parse(process.env);
@@ -28,5 +31,10 @@ export default {
     url: envVars.REDIX_URL
   },
   authServiceUrl: envVars.AUTH_SERVICE_URL,
-  corServiceUrl: envVars.COR_SERVICE_URL
+  corServiceUrl: envVars.COR_SERVICE_URL,
+  cloudinary: {
+        cloudName: envVars.CLOUDINARY_CLOUD_NAME,
+        apiKey: envVars.CLOUDINARY_API_KEY,
+        apiSecret: envVars.CLOUDINARY_API_SECRET
+    }
 };
